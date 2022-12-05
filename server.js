5b6dceaf6e3dbc58f5e29fd24fc49bcbe119ae79
Efe Polat <mefepolat@hotmail.com>
2022-12-05 14:16:33 -0500

    added get single user route

--- server.js
+++ server.js
@@ -29,6 +29,18 @@ app.get("/api/users", (req, res) => {
 // /api/users/1      <-- path param (req.params.id)
 // /api/users?id=1   <-- query param (req.query.id) If you go with query param, just modify the existing endpoint above instead of creating a new endpoint
 
+app.get("/api/users/:id",(req,res) => {
+  const id = req.params.id;
+  console.log(id)
+  const user = data.find(user => {
+    if(user.id == id){
+      return user;
+    }
+  })
+  console.log(user);
+  res.end();
+})
+
 // BONUS QUESTION - Add routes to implement all the CRUD operations (POST, PUT, DELETE)
 
 app.listen(3000, () => {
